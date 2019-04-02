036bcfba74c17d7ce2a429666ce38a5908ca4153
Nstats <17801238715@163.com>
2019-04-02 20:19:59 +0800

    4.2

--- run_squad2_train.sh
+++ run_squad2_train.sh
@@ -7,10 +7,12 @@ python run_squad.py \
 --train_file=./data/squad/train-v2.0.json \
 --do_predict=False \
 --predict_file=./data/squad/dev-v2.0.json \
+--output_dir=./data/squad/layer-1 \
 --train_batch_size=12 \
 --learning_rate=3e-5 \
 --num_train_epochs=1 \
---max_seq_length=3 \
+--max_seq_length=300 \
+--max_query_length=30 \
 --version_2_with_negative=True \
 --max_answer_length=15 \
 --null_score_diff_threshold=0.0 \
