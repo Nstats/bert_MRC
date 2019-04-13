d5ba6f6056996d1b540f7051c9909a0664ccfca8
Nstats <17801238715@163.com>
2019-04-13 16:38:21 +0800

    4.12

--- run_squad2_eval.sh
+++ run_squad2_eval.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 folder_name=use_trainable_word2vec
-num_prediction=17
+num_prediction=5
 
 for ((i=0; i<${num_prediction}; i++)); do
     predict_dir=./data/squad/${folder_name}/${i}_predictions.json
